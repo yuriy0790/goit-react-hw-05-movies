8aee6ba898b231ab6698b0a6a41e68fc5674d722
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-01-18 06:14:57 +0200

    fix: movie-info styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import { GlobalStyle } from './styles/GlobalStyle';
 

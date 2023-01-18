57c9693316748a2ce375d2c752e218b0312f1eef
yuriy0790 <90905066+yuriy0790@users.noreply.github.com>
2023-01-18 15:47:34 +0000

    Deploying to gh-pages from @ yuriy0790/goit-react-hw-05-movies@c71e8adcbf5c8ba9ab9ee2f476ce33cfcfd2f2c9 🚀

---
+++ static/js/425.ccf7aa40.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[425],{7425:function(t,a,e){e.d(a,{TPM:function(){return h}});var r=e(9983);function h(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M8 7v4L2 6l6-5v4h5a8 8 0 1 1 0 16H4v-2h9a6 6 0 1 0 0-12H8z"}}]}]})(t)}}}]);
+//# sourceMappingURL=425.ccf7aa40.chunk.js.map
